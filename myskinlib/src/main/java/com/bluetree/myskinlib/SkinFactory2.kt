49d19ce0374fc78a16379f6e720bc2d8455f009c
GavinHuang <ping798370123@126.com>
2019-10-09 18:32:24 +0800

    开发换肤功能

--- myskinlib/src/main/java/com/bluetree/myskinlib/SkinFactory2.kt
+++ myskinlib/src/main/java/com/bluetree/myskinlib/SkinFactory2.kt
@@ -14,20 +14,27 @@ class SkinFactory2 : LayoutInflater.Factory2 {
 
     val widgetString = arrayOf("android.view.","android.widget.","android.webkit.")
     override fun onCreateView(parent: View?, name: String?, context: Context?, attrs: AttributeSet?): View {
-
-
         LogUtils.i(this, name)
 
+        var view = null
+        for (s in widgetString) {
+            if (name!!.contains(s)) {
+                onCreateView(name, context, attrs)
+            }else{
+                break
+            }
+        }
+        if (view == null) {
+            onCreateView(name, context, attrs)
+        }
 
-        return onCreateView(name, context, attrs)
+        return view
     }
 
     override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View {
-
         val item  = context?.getClassLoader()?.loadClass(name);
         var construtor:Constructor<View> = item?.getConstructor(Context::class.java, AttributeSet::class.java) as Constructor<View>
         var view = construtor?.newInstance(context, attrs)
-
         return view
 
     }
